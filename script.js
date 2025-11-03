f6804f97fed747943f48a49fa24749d9b7efef7b
AnswerKeyWali <excelwali365@gmail.com>
2025-11-03 07:22:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// data.js
+
+// Paste your 160 official correct answers in order below (example shown)
+const ANSWER_KEY = "ABBCCDAABCD...";  // replace with real 160 answers
+
+const TOTAL_QUESTIONS = 160;
+const MARK_PER_QUESTION = 1.25;
+const NEGATIVE_MARK = MARK_PER_QUESTION / 3; // 1/3 negative marking
